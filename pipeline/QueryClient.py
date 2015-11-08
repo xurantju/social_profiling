4e4210f8280efca916d3947c6a7d9a0551551d4f
Caiming Xiong <cmxiong@caiming-MBP.local>
2015-11-07 19:30:00 -0800

    c/s

---
+++ pipeline/QueryClient.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python (CLIENT CODE)
+
+import httplib
+import sys
+
+#get http server ip
+http_server = sys.argv[1]
+#create a connection
+conn = httplib.HTTPConnection(http_server,1234)
+
+while 1:
+            cmd = raw_input('input command (ex. GET index.py): ')
+            cmd = cmd.split()
+
+            if cmd[0] == 'exit': #tipe exit to end it
+                        break
+
+            #request command to server
+            conn.request(cmd[0], cmd[1])
+
+
+            #get response from server
+            rsp = conn.getresponse()
+
+            #print server response and data
+            print(rsp.status, rsp.reason)
+            data_received = rsp.read()
+            print(data_received)
+
+conn.close
